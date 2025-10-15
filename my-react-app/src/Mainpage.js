0bee2f8ac3dcd208865b483abb2308455bc3e15e
muhammad talal <talalilyas11@gmail.com>
2025-10-15 16:15:04 +0500

    improvements

--- my-react-app/src/Mainpage.js
+++ my-react-app/src/Mainpage.js
@@ -1,10 +1,8 @@
-export default function Mainpage ()  {
-
-
-    return(
-
-
-
-        <div> <h1>today i start working on the my portfolio</h1></div>
-    )
+export default function Mainpage() {
+  return (
+    <div>
+      {" "}
+      <h1>today i start working on the my portfolio</h1>
+    </div>
+  );
 }
