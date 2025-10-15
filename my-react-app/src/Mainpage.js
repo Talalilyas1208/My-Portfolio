a066e4ec5cd0c882fa2bd1a5e88f854193fea2f5
muhammad talal <talalilyas11@gmail.com>
2025-10-15 17:45:13 +0500

    improvements : in coee

--- my-react-app/src/Mainpage.js
+++ my-react-app/src/Mainpage.js
@@ -1,28 +1,36 @@
-import { Layout, Button, Typography, Row, Col, Space } from "antd";
+import { Layout, Button, Typography, Row, Col, Space, Card, Input } from "antd";
 
-const { Header } = Layout;
+const { Header, Content, Footer } = Layout;
 const { Title } = Typography;
 
 export default function AppHeader() {
   return (
-    <Header>
-      <Row justify="space-between" align="middle">
-    
-        <Col>
-          <Title level={4} style={{ color: "inherit", margin: 0 }}>
-        Frontend devloper   
-          </Title>
-        </Col>
-        <Col>
-          <Space>
-            <Button  type="text">About me</Button>
-             <Button  type="text">Experience</Button>
-              <Button  type="text">Project</Button>
-               <Button  type="text">contact me</Button>
-            <Button  type="text">Sign Up</Button>
-          </Space>
-        </Col>
-      </Row>
-    </Header>
+    <Layout>
+      <Header>
+        <Row justify="space-between" align="middle">
+          <Col>
+            <Title level={4} style={{ color: "inherit", margin: 0 }}>
+              Frontend devloper
+            </Title>
+          </Col>
+          <Col>
+            <Space>
+              <Button type="text">About me</Button>
+              <Button type="text">Experience</Button>
+              <Button type="text">Project</Button>
+              <Button type="text">contact me</Button>
+              <Button>Download Cv</Button>
+            </Space>
+          </Col>
+        </Row>
+      </Header>
+
+      <Content>
+        <Space
+          direction="vertical"
+          style={{ width: "100%", padding: 24 }}
+        ></Space>
+      </Content>
+    </Layout>
   );
 }
