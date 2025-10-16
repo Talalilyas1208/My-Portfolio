9330e64fd368d91d604831c4d546b12116f4ab82
muhammad talal <talalilyas11@gmail.com>
2025-10-16 17:42:08 +0500

    improvements in code

--- my-react-app/src/Mainpage.js
+++ my-react-app/src/Mainpage.js
@@ -1,36 +1,61 @@
-import { Layout, Button, Typography, Row, Col, Space, Card, Input } from "antd";
+import { Layout, Button, Typography, Row, Col, Space, ConfigProvider, theme ,Card} from "antd";
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
 const { Title } = Typography;
 
 export default function AppHeader() {
   return (
-    <Layout>
-      <Header>
-        <Row justify="space-between" align="middle">
-          <Col>
-            <Title level={4} style={{ color: "inherit", margin: 0 }}>
-              Frontend devloper
-            </Title>
-          </Col>
-          <Col>
-            <Space>
-              <Button type="text">About me</Button>
-              <Button type="text">Experience</Button>
-              <Button type="text">Project</Button>
-              <Button type="text">contact me</Button>
-              <Button>Download Cv</Button>
-            </Space>
-          </Col>
-        </Row>
-      </Header>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          colorBgBase: "#0f0f0f",
+          colorTextBase: "#ffffff",
+          borderRadius: 8,
+        },
+      }}
+    >
+      <Layout >
+      
+        <Header
+        
+        >
+          <Row justify="space-between" align="middle" style={{backgroundColor :"#0f0f0f"}}>
+            <Col lg={16}>
+              <Title level={4} style={{ color: "#fff", margin: 0 }}>
+                Frontend Developer
+              </Title>
+            </Col>
+            <Col lg={8}> <Card  variant="borderless" style={{ width: 600 ,padding:"1px" }}>
+              <Space>
+                <Button type="text" >
+                  About Me
+                </Button>
+                <Button type="text" >
+                  Experience
+                </Button>
+                <Button type="text">
+                  Projects
+                </Button>
+                <Button type="text">
+                  Contact Me
+                </Button>
+                <Button >Download CV</Button>
+              </Space>
+              </Card>
+            </Col>
+          </Row>
+        </Header>
 
-      <Content>
-        <Space
-          direction="vertical"
-          style={{ width: "100%", padding: 24 }}
-        ></Space>
-      </Content>
-    </Layout>
+        <Content
+          style={{
+            backgroundColor: "#0f0f0f",
+          
+          }}
+        >
+          <h1>This is my new project</h1>
+        </Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
