d9c94da01085b3bd338f82f265c4af0ff8cd1227
muhammad talal <talalilyas11@gmail.com>
2025-10-16 17:41:25 +0500

    update routing without the custom color

--- my-react-app/src/Routing.js
+++ my-react-app/src/Routing.js
@@ -4,17 +4,10 @@ import { ConfigProvider, theme } from "antd";
 
 export default function Routing() {
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm, // built-in dark mode
-        token: {
-          borderRadius: 8, // smooth corners
-        },
-      }}
-    >
+
       <Routes>
         <Route index element={<Mainpage />} />
       </Routes>
-    </ConfigProvider>
+ 
   );
 }
